7a54d55cff1870eb4c9b2c0690cde09401af1b1b
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2021-08-14 09:00:55 +0900

    tailwidocss設定終わって使用中

---
+++ components/layout.js
@@ -0,0 +1,8 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ children, title = "HP by Nextjs" }){
+  return(
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono"></div>
+  );
+}
